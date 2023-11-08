1c5fb0ad2850232640e6c221735ee05aac956ba3
gilhoyun <gilhoyun04@gmail.com>
2023-11-08 12:47:55 +0900

    list, detail에 작성자이름, include사용법

--- src/main/java/com/koreaIT/java/am/ArticleDoWriteServlet.java
+++ src/main/java/com/koreaIT/java/am/ArticleDoWriteServlet.java
@@ -1,11 +1,14 @@
 package com.koreaIT.java.am;
+
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
 import com.koreaIT.java.am.config.Config;
 import com.koreaIT.java.am.util.DBUtil;
 import com.koreaIT.java.am.util.SecSql;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -16,22 +19,24 @@ import jakarta.servlet.http.HttpSession;
 @WebServlet("/article/doWrite") // url매핑
 public class ArticleDoWriteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		response.setContentType("text/html; charset=UTF-8;");
 		
 		String title = request.getParameter("title");
 		String body = request.getParameter("body");
-
+		
 		HttpSession session = request.getSession();
 		int loginedMemberId = (int) session.getAttribute("loginedMemberId");
-
+		
 		Connection conn = null;
 
 		try {
 			Class.forName(Config.getDBDriverName());
 			String url = Config.getDBUrl();
 			conn = DriverManager.getConnection(url, Config.getDBUser(), Config.getDBPassWd());
+
 			SecSql sql = new SecSql();
 			sql.append("INSERT INTO article");
 			sql.append("SET regDate = NOW(),");
@@ -39,7 +44,7 @@ public class ArticleDoWriteServlet extends HttpServlet {
 			sql.append("memberId = ?,", loginedMemberId);
 			sql.append("title = ?,", title);
 			sql.append("body = ?", body);
-
+			
 			int id = DBUtil.insert(conn, sql);
 			
 			response.getWriter().append(String.format("<script>alert('%d번 게시물이 생성되었습니다'); location.replace('detail?id=%d');</script>", id, id));
@@ -64,4 +69,5 @@ public class ArticleDoWriteServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		doGet(req, resp);
 	}
+
 }
